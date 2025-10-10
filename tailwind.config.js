d8596211e5bbe1aa7274046ec0de63ec1f3e97e5
Sreejith.R <shrsree341o@gmail.com>
2025-10-11 03:29:06 +0530

    new start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
-import {nextui} from '@nextui-org/theme'
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
-  ],
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [nextui()],
-}
+  plugins: [],
+};
